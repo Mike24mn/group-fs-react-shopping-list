04e73bfaf24763a26923089658e3503f726fe9ec
Joshua Bass <joshuabass77@gmail.com>
2024-05-30 13:11:40 -0500

    added axios GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import React from 'react';
 
 import Header from '../Header/Header.jsx'
@@ -5,11 +7,32 @@ import './App.css';
 
 
 function App() {
+    const [shoppingList, setShoppingList]= useState([]);
+
+    useEffect( () => {
+        fetchList();
+      }, [])
+
+      const fetchList =() => {
+        axios({
+            method: "GET",
+            url: "./api/food"
+        })
+        .then((response) =>{
+            console.log('response.data is:', response.data);
+
+            setShoppingList(response.data)
+        })
+        .catch((error) => {
+            console.log('Error on get:', error);
+          });
+      }
+
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <p>yo momma ...</p>
             </main>
         </div>
     );
